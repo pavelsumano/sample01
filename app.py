b7fa0763ea0a180075d9dcc770861a83aa6364ba
pavelsumanoo <sumanoop@gmail.com>
2020-12-03 01:19:37 -0600

    simple change

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from chalice import Chalice
 app = Chalice(app_name='sample01')
 
 
-@app.route('/')
-def index():
-    return {'hello': 'pavel'}
+# @app.route('/')
+# def index():
+#     return {'hello': 'Pavel'}
 
 
 # The view function above will return {"hello": "world"}
@@ -18,12 +18,12 @@ def index():
 #    # '/hello/james' -> {"hello": "james"}
 #    return {'hello': name}
 #
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
+@app.route('/users', methods=['POST'])
+def create_user():
+    # This is the JSON body the user sent in their POST request.
+    user_as_json = app.current_request.json_body
+    # We'll echo the json body back to the user in a 'user' key.
+    return {'user': user_as_json}
 #
 # See the README documentation for more examples.
 #
